f5af4e42ac7e64a01e733c6a8746184b873a1b45
Kunal Patil <kpatil@mobiquityinc.com>
2020-02-07 12:51:48 +0530

    Docker java demo
    Docker java demo

---
+++ HelloDocker.java
@@ -0,0 +1,18 @@
+/**
+ * @author kpatil
+ *
+ */
+public class HelloDocker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("\n\n=========================================================\n");
+		System.out.print("\t\tHellow world from docker");
+		System.out.print("\n=========================================================\n\n");
+
+	}
+
+}
