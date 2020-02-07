d03f0b41715dec20272e4cc45fdee6d4d830e02d
Kunal Patil <kpatil@mobiquityinc.com>
2020-02-07 13:39:56 +0530

    updated docker demo
    updated docker demo

--- HelloDocker.java
+++ HelloDocker.java
@@ -10,7 +10,7 @@ public class HelloDocker {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("\n\n=========================================================\n");
-		System.out.print("\t\tHellow world from docker");
+		System.out.print("\t\tHellow world from docker demo");
 		System.out.print("\n=========================================================\n\n");
 
 	}
